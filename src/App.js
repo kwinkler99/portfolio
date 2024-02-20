79a4d7da9cb4402f908f35fab001f35bad6056d4
kasia <kasia.winkler99@gmail.com>
2024-02-20 22:45:41 +0100

    Attach new pages to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import "./assets/style/app.scss"
+
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+import Home from "./pages/Home";
+import Snake from "./pages/Snake";
+
 function App() {
   return (
+    <>
     <div className="App">
+      <Home />
+      <Experience />
+      <Snake />
+      <Contact />
     </div>
+    </>
   );
 }
 
