f2c514f691b79770736b996430aad6e77a40561b
kasia <kasia.winkler99@gmail.com>
2024-02-20 22:43:41 +0100

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import "../assets/style/home.scss";
+
+import BackHandRoundedIcon from "@mui/icons-material/BackHandRounded";
+import LaptopMacRoundedIcon from "@mui/icons-material/LaptopMacRounded";
+
+const Home = () => {
+  const firstLinePartOne = "Hi";
+  const firstLinePartTwo = "my name is";
+  const myNameAndSurname = "Katarzyna Winkler";
+  const secondLine = "and I do stuff for websites";
+
+  return (
+    <span className="home">
+      <span className="first-line">
+        <p>{firstLinePartOne}</p>
+        <BackHandRoundedIcon />
+        <p>{firstLinePartTwo}</p>
+      </span>
+      <span className="name">
+        <p>{myNameAndSurname}</p>{" "}
+      </span>
+      <span className="second-line">
+        <p>{secondLine}</p>
+        <LaptopMacRoundedIcon />
+      </span>
+    </span>
+  );
+};
+
+export default Home;
