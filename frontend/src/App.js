5d49827182b0e30f0a37808544db2d1f31d5e4e2
kasia <kasia.winkler99@gmail.com>
2024-03-04 13:37:18 +0100

    Create a mui theme for the buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,32 @@
 import "./assets/style/app.scss"
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import Navbar from "./Navbar";
 import Contact from "./pages/Contact";
 import Experience from "./pages/Experience";
 import Home from "./pages/Home";
 import Snake from "./pages/Snake";
 
+const buttonsTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#B29758",
+    },
+  },
+});
+
 function App() {
   return (
     <>
       <Navbar />
       <div className="App">
-        <Home />
-        <Experience />
-        <Snake />
-        <Contact />
+        <ThemeProvider theme={buttonsTheme}>
+            <Home />
+            <Experience />
+            <Snake />
+            <Contact />
+        </ThemeProvider>
       </div>
     </>
   );
