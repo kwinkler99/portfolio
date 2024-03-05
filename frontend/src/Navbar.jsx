beb272742142dc68bc0ca91cfd3b058f1788805a
kasia <kasia.winkler99@gmail.com>
2024-03-05 23:33:22 +0100

    Switch navbar between default and mobile

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,12 +1,16 @@
 import "./assets/style/navbar.scss";
 import logo from "./assets/images/Logo.png";
+import darkLogo from "./assets/images/LogoDark.png";
 
 import { useState, useEffect, useRef } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import MenuIcon from "@mui/icons-material/Menu";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState(null);
+  const [openMobileNavbar, setOpenMobileNavbar] = useState(false);
   const sections = useRef([]);
 
   const handleScroll = () => {
@@ -41,9 +45,57 @@ const Navbar = () => {
     textDecoration: "underline var(--gold)",
   };
 
+  const mobileActiveStyle = {
+    textDecoration: "underline var(--dark-blue)",
+  };
+
+  const navbarList = [
+    {
+      title: "Experience",
+      section: "experience",
+    },
+    {
+      title: "Let's play!",
+      section: "snake",
+    },
+    {
+      title: "Contact",
+      section: "contact",
+    },
+  ];
+
+  const DefaultList = () =>
+    navbarList.map((item) => (
+      <li
+        key={item.section}
+        onClick={() =>
+          document.getElementById(item.section).scrollIntoView(true)
+        }
+        id={`nav-button-${item.section}`}
+        style={activeSection === item.section ? activeStyle : {}}
+      >
+        {item.title}
+      </li>
+    ));
+
+  const MobileList = () =>
+    navbarList.map((item) => (
+      <li
+        key={item.section}
+        onClick={() => {
+          document.getElementById(item.section).scrollIntoView(true);
+          setOpenMobileNavbar(false);
+        }}
+        id={`nav-button-${item.section}`}
+        style={activeSection === item.section ? mobileActiveStyle : {}}
+      >
+        {item.title}
+      </li>
+    ));
+
   return (
     <>
-      <nav>
+      <nav className="default-navbar">
         <span className="logo">
           <img
             onClick={() => document.getElementById("home").scrollIntoView(true)}
@@ -53,36 +105,51 @@ const Navbar = () => {
             height={40}
           />
         </span>
-        <ul className="default-navbar">
-          <li
-            onClick={() =>
-              document.getElementById("experience").scrollIntoView(true)
-            }
-            id="nav-button-experience"
-            style={activeSection === "experience" ? activeStyle : {}}
-          >
-            Experience
-          </li>
-          <li
-            onClick={() =>
-              document.getElementById("snake").scrollIntoView(true)
-            }
-            id="nav-button-snake"
-            style={activeSection === "snake" ? activeStyle : {}}
-          >
-            Let's play!
-          </li>
-          <li
-            onClick={() =>
-              document.getElementById("contact").scrollIntoView(true)
-            }
-            id="nav-button-contact"
-            style={activeSection === "contact" ? activeStyle : {}}
-          >
-            Contact
-          </li>
+        <ul>
+          <DefaultList />
         </ul>
       </nav>
+
+      <nav className="mobile-navbar">
+        <span className="logo">
+          <img
+            onClick={() =>
+              !openMobileNavbar &&
+              document.getElementById("home").scrollIntoView(true)
+            }
+            src={openMobileNavbar ? darkLogo : logo}
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+        </span>
+        {!openMobileNavbar ? (
+          <MenuIcon onClick={() => setOpenMobileNavbar(true)} />
+        ) : (
+          <ClearIcon
+            className="clear-icon"
+            onClick={() => setOpenMobileNavbar(false)}
+          />
+        )}
+      </nav>
+
+      {openMobileNavbar && (
+        <span className="modal-navbar">
+          <ul>
+            <li
+              onClick={() => {
+                document.getElementById("home").scrollIntoView(true);
+                setOpenMobileNavbar(false);
+              }}
+              id={`nav-button-home`}
+              style={activeSection === "home" ? mobileActiveStyle : {}}
+            >
+              My name is...
+            </li>
+            <MobileList />
+          </ul>
+        </span>
+      )}
 
       {(activeSection === "experience" || activeSection === "contact") && (
         <span className="arrow-mobile-up">
